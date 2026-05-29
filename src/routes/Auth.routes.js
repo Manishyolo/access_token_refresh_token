e0c04cf942471bd0bccbe96585253c545b8a7c4f
Manish kumar <manish04kumarsingh@gmail.com>
2026-05-29 10:10:51 +0530

    feat: update CORS origin and add user retrieval route with authentication middleware

--- src/routes/Auth.routes.js
+++ src/routes/Auth.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const {LoginController,RegisterController,getAccessTokenController} = require('../controllers/auth.controller.js');
-
+const authMiddleware = require('../middleware/auth.middleware.js');
 
 const router = express.Router();
 
+router.get("/me",authMiddleware,(req,res)=>{
+    return res.status(200).json({message:"current loged in user",user:req.user})
+})
 router.post("/login", LoginController);
 router.post("/register", RegisterController);
 router.get("/get-accessToken",getAccessTokenController);
